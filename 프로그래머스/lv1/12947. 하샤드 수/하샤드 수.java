92bb46fc9b3bd588e7ee43bdd20ed83942bb73eb
GS-Oh <97492296+GS-Oh@users.noreply.github.com>
2022-12-07 20:41:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public boolean solution(int x)
+    {
+        int temp = x;
+        int sum = 0;
+        
+        while (temp >= 10)
+        {
+            sum += temp % 10;
+            
+            temp /= 10;
+        }
+        
+        sum += temp;
+        
+        return x % sum == 0;
+    }
+}
