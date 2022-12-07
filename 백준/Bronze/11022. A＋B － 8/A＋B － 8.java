27c727e01724e881fd4a3a2da6de7fe24f67ac91
GS-Oh <97492296+GS-Oh@users.noreply.github.com>
2022-12-07 20:25:15 +0900

    [Bronze V] Title: A+B - 8, Time: 268 ms, Memory: 18916 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args){
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=1; i<=N; i++) {
+			int A,B;
+			A= sc.nextInt();
+			B= sc.nextInt();
+			
+			System.out.println("Case #"+ i + ": " +A +" + "+B+" = "+(A+B));
+		}
+
+		sc.close();
+    }
+}
