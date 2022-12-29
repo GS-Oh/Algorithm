138d4e132dd34c0ef42337520e93c1cd2d0f36ed
GS-Oh <97492296+GS-Oh@users.noreply.github.com>
2022-12-29 14:48:41 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        int max = 0;
+        
+        for(int i =0; i<array.length; i++){
+            if(max < array[i]){
+                max = array[i];
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
