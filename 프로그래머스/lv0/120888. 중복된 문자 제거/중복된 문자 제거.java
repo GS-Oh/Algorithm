335faec562aece93bb8fa29099646c40cbb9596c
GS-Oh <97492296+GS-Oh@users.noreply.github.com>
2022-12-29 14:46:31 +0900

    [level 0] Title: 중복된 문자 제거, Time: 11.11 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        char[] c = my_string.toCharArray();
+		for (char x : c) {
+			if (!answer.contains(String.valueOf(x))) answer += x;
+		}
+        
+        return answer;
+    }
+}
