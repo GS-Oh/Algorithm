add0be61b34bd08181c55dcb3b834e11bc083ee4
GS-Oh <97492296+GS-Oh@users.noreply.github.com>
2023-01-03 21:20:38 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey != 0) {
+
+			if(storey%10 > 5 ) {
+				answer += (10 -(storey%10));
+				storey += 10;
+			}else if(storey%10 == 5 && storey%100 != 0 && storey%100 >= 50) {
+				answer += 5;
+				storey += 10;
+			}else {
+				answer += storey%10;
+			}
+			
+			storey /= 10;
+		}
+	    
+  
+        return answer;
+    }
+}
